1ba99dbc596d047c14458ccc75c289bba6903a2a
Daniel Espinosa <d111092.de@gmail.com>
2021-12-09 21:29:02 -0600

    refactor: remove env variables

--- database/config.js
+++ database/config.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect('mongodb+srv://root:root@cluster0.jc3pb.mongodb.net/players');
+        await mongoose.connect(process.env.MONGODB_CONN);
 
         console.log('DB online!');
     } catch (error) {
